9d4fb7a74cbde7ca88f9e4871db2ba5f97b877ee
pravinmobilefirst9 <81845996+pravinmobilefirst9@users.noreply.github.com>
2023-12-22 14:43:28 +0530

    Added AuthGuard Module in Providers
    Added AuthGuard Module in Providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma.service';
 import { PlaidTartanModule } from './plaid-tartan/plaid-tartan.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [UsersModule, PlaidTartanModule, TransactionModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, AuthGuard],
 })
 export class AppModule {}
