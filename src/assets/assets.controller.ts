0c370556502d2dc8050def9ffd3a3037c24cf762
unknown <rohangaikwad9881@gmail.com>
2024-01-03 11:07:49 +0530

    asset controller updated

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -8,17 +8,16 @@ import { AssetFormDetails } from './dto/asset-form.dto';
 
 @Controller('assets')
 export class AssetsController {
-  constructor(private readonly assetsService: AssetsService) {}
+  constructor(private readonly assetsService: AssetsService) { }
 
 
   @Post("create_asset_report_token")
   @UseGuards(AuthGuard)
   async createAssetReportToken(
-    @Req() request : any,
-  )
-  {
-    try {     
-      const {user_id} = request.auth     
+    @Req() request: any,
+  ) {
+    try {
+      const { user_id } = request.auth
       const result = await this.assetsService.createAssetReportToken(user_id);
       return result;
     } catch (error) {
@@ -30,11 +29,10 @@ export class AssetsController {
   @Post("import_asset_reports")
   @UseGuards(AuthGuard)
   async importAssets(
-    @Req() request : any,
-  )
-  {
-    try {  
-      const {user_id} = request.auth     
+    @Req() request: any,
+  ) {
+    try {
+      const { user_id } = request.auth
       const result = await this.assetsService.importAssetReports(user_id);
       return result;
     } catch (error) {
@@ -45,45 +43,45 @@ export class AssetsController {
   @Get('/get_assets_details')
   @UseGuards(AuthGuard)
   async getAssetDetails(@Req() req: any) {
-    const {user_id} = req.auth   
+    const { user_id } = req.auth
     return await this.assetsService.getAssetDetails(user_id);
   }
 
   @Get("assets_list")
   @UseGuards(AuthGuard)
-  async getAssetsList(){
+  async getAssetsList() {
     return await this.assetsService.getAssetsLists();
   }
 
   @Post("asset_sub_types")
   @UseGuards(AuthGuard)
   async getAssetsSubTypes(
-    @Body() data : AssetsSubTypeDTO
-  ){
-   
+    @Body() data: AssetsSubTypeDTO
+  ) {
+
     return await this.assetsService.getAllAssetsSubTypes(data.asset_id);
   }
 
   @Post("get_asset_form")
   @UseGuards(AuthGuard)
   async getAssetForm(
-    @Req() request : any,
-    @Body() data : AssetFormDetails
-  ){
-    const {user_id} = request.auth    
-    return await this.assetsService.getFormData(data, user_id); 
+    @Req() request: any,
+    @Body() data: AssetFormDetails
+  ) {
+    const { user_id } = request.auth
+    return await this.assetsService.getFormData(data, user_id);
   }
 
   @Post("create_asset")
   @UseGuards(AuthGuard)
   async addAsset(
-    @Req() request : any,
-    @Body() data : CreateAssetDto
-  ){
-    const {user_id} = request.auth;
+    @Req() request: any,
+    @Body() data: CreateAssetDto
+  ) {
+    const { user_id } = request.auth;
     return await this.assetsService.addUserAssetsDetails(data, user_id)
-    }
-  
+  }
+
   @Post()
   create(@Body() createAssetDto: CreateAssetDto) {
     return this.assetsService.create(createAssetDto);
