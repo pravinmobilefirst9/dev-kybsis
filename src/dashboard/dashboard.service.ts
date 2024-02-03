55c2b189714492a510424883901f1d1b1c161a0d
unknown <rohangaikwad9881@gmail.com>
2024-02-03 15:22:14 +0530

    Fixed widgets fetch issue for premium users

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -182,7 +182,8 @@ export class DashboardService {
 
           resultObj = await this.prisma.userWidgets.findMany({
             where : {
-              user_id
+              user_id,
+              active : true
             },
             select : {
               id : true,
