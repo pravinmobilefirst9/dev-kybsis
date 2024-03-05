381369b7eb91b274ba003190a4409b193b9527ca
unknown <rohangaikwad9881@gmail.com>
2024-03-05 18:38:00 +0530

    stripe service updated

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from "@nestjs/common";
 
 
 
-const  ProductIdWithRole = {
-  prod_PbPAM3TNohple7 : "PREMIUM",
-  prod_PbPAaKx3F2jl88 : "BASIC"
+const ProductIdWithRole = {
+  prod_PbPAM3TNohple7: "PREMIUM",
+  prod_PbPAaKx3F2jl88: "BASIC"
 }
 
 @Injectable()
 export class StripeService {
 
- 
 
-    
-  
+
+
+
 }
